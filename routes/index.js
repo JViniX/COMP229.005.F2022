5e161d199a00555b7b5cc1cebf81c01245a4a1ed
JViniX <47263194+JViniX@users.noreply.github.com>
2022-10-08 12:35:48 -0400

    1.5.1

--- routes/index.js
+++ routes/index.js
@@ -4,36 +4,15 @@
 
 var express = require('express');
 var router = express.Router();
+let indexController = require('../controllers/index.controller');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render(
-    'index', 
-    { 
-      title: 'Home', 
-      name: 'Julio' 
-    }
-  );
-});
+router.get('/', indexController.home);
 
 // Renders the projects page
-router.get('/projects', function(req, res, next) {
-  res.render(
-    'projectservices', 
-    { 
-      title: 'Projects',
-    }
-  );
-});
+router.get('/projects', indexController.projects);
 
-router.get('/services', function(req, res, next) {
-  res.render(
-    'projectservices', 
-    { 
-      title: 'Services',
-    }
-  );
-});
+router.get('/services', indexController.services);
 
 router.get('/about', function(req, res, next) {
   res.render(
