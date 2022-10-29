f1f5d2a61ec7310a5324013c648b262d07abe572
JViniX <47263194+JViniX@users.noreply.github.com>
2022-10-29 12:25:42 -0400

    Week 07 final

--- config/db.js
+++ config/db.js
@@ -1,12 +1,11 @@
-// In real project, never expose your credential in your code.
-let atlasDB = "mongodb+srv://dbuser:YmAtl2iVPIzhH4V6@cluster005.vkuwqmu.mongodb.net/products";
+let config = require('./config');
 
 let mongoose = require('mongoose');
 
 module.exports = function(){
 
     // Connect to the database
-    mongoose.connect(atlasDB);
+    mongoose.connect(config.ATLASDB);
 
     let mongodb = mongoose.connection;
     mongodb.on('error', console.error.bind(console, 'Connection Error:'));
--- controllers/index.js
+++ controllers/index.js
@@ -1,23 +1,9 @@
 exports.home = function(req, res, next) {
     
-  res.status(200).json(
+  res.json(
     {
       success: true,
       message: "This is the home endpoint."
     }
   )
 };
-
-exports.about = function(req, res, next) {
-  res.render('index', { 
-      title: 'About',
-      userName: req.user ? req.user.username : ''
-   });
-}
-
-exports.projects = function(req, res, next) {
-  res.render('index', { 
-    title: 'Projects',
-    userName: req.user ? req.user.username : '' 
-  });
-}
